b707699826a4ca51aa5bab0420e19154c35ce909
balqui <jose.luis.balcazar@upc.edu>
2021-06-02 18:53:42 +0200

    Uploading to GitHub local files for first working version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # guesspresso
 Expression evaluation game
+
+Using Brython to play the Expression Game.
+
+Currently on Brython-3.8.0.
+
+José L Balcázar (balqui at GitHub), spring and summer 2021.
+
+Put the files somewhere a web server can see and open index.html,
+the rest should be self-explanatory.
+
+
---
+++ guesspresso.py
@@ -0,0 +1,41 @@
+# ~ <script type="text/python">
+from browser import document, alert # Brython in-browser support
+# ~ from store import Store
+from operator import *
+
+about_str = """GuessPresso:
+
+Using Brython to play the Expression Game.
+
+Currently on Brython-3.8.0.
+
+José L Balcázar (balqui at GitHub), spring and summer 2021."""
+
+
+def about(ev):
+    alert(about_str)
+
+def done(ev):
+	"capture contents of in and out, eval in, compare to out, choose alert contents"
+	# ~ alert("Done called")
+	e = ''.join(document["in"].value.split())
+	# ~ alert("read: " + e)
+	try:
+		v = repr(eval(e))
+	except Exception as exc:
+		v = "Error"
+	w = document["out"].value
+	if v == w:
+		alert("Correct guess!")
+	elif v.strip() == w.strip():
+		alert("Correct guess, but be careful with spaces...")
+	else:
+		alert("No, sorry: " + w + " is wrong; outcome is " + v + ".")
+	if v == "Error":
+		alert("The type of error was: " + str(type(exc))[7:-1])
+
+# main program: 
+# bind buttons to processes and leave interaction for Brython to care for.
+
+document['about'].bind('click', about)
+document['done'].bind('click', done)
